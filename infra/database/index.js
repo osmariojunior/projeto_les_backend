739cf887c5afe680b7e83d72c1b3d2c8389976c2
Felipe Hsu <felipe_hsu@hotmail.com>
2022-04-11 10:35:43 -0300

    feat: modified procfile

--- infra/database/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = require("knex")({
-  client: "pg",
-  connection: {
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
-  },
-});
--- src/controllers/users/register/request-schema.js
+++
@@ -1,9 +0,0 @@
-const yup = require("yup");
-
-module.exports = yup.object().shape({
-  firstName: yup.string().max(255).required(),
-  lastName: yup.string().max(255).required(),
-  email: yup.string().email().max(255).required(),
-  password: yup.string().min(6).max(64).required(),
-  nickname: yup.string().min(6).max(64).required(),
-});
